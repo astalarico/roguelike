052a0c0fc195cff4934fab7af4dda7db651d30d5
Anthony Talarico <astalarico@gmail.com>
2023-03-08 20:48:16 -0500

    WIP : troubleshooting astargrid2D issues

---
+++ Scripts/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+@onready var ray = $RayCast2D
+signal update_world 
+
+func _unhandled_input(event)->void:
+	for direction in state.directions:
+		if event.is_action_pressed(direction):
+			move(direction)
+			
+			
+func move( direction )->void:
+	var vector_position = state.directions[direction] * state.GRID_CELL_SIZE
+	#print("vector position" + str(self.position))
+	ray.target_position = vector_position
+	#print("PLAYER VECTOR POSITION FOR RAY CAST " + str(vector_position ))
+	ray.force_raycast_update()
+
+	if !ray.is_colliding():
+		self.position += vector_position
+		emit_signal("update_world" )
+	
+	else:
+		var nodeObj = ray.get_collider();
+		if( nodeObj.has_method('_interact')):
+			nodeObj.call('_interact', direction)
+			
+		#print( ray.get_collision_point() )
+		print( nodeObj.get('position') )
+		
+
+func _ready()->void:
+	emit_signal("update_world")
---
+++ Scripts/state.gd
@@ -0,0 +1,31 @@
+extends Node
+
+const GRID_CELL_SIZE = 16
+var directions = {
+	'ui_up' : Vector2.UP,
+	'ui_down' : Vector2.DOWN,
+	'ui_right' : Vector2.RIGHT,
+	'ui_left' : Vector2.LEFT
+}
+
+@onready var player = get_tree().get_root().get_node('Game/Player')
+@onready var world = get_tree().get_root().get_node('Game/World')
+@onready var path_line = get_tree().get_root().get_node('Game/World/Line2D')
+
+var astar_grid = AStarGrid2D.new()
+var world_tiles
+var world_rect
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	path_line.width = 1.0
+	world_tiles = world.get_used_cells(0)
+	world_rect = world.get_used_rect()
+	astar_grid.size = Vector2i(world_rect.size)
+	astar_grid.default_compute_heuristic = 3
+	astar_grid.cell_size = Vector2(1,1)
+	astar_grid.offset = Vector2(world_rect.position)
+	print( astar_grid.points )
+	astar_grid.diagonal_mode = 1
+	astar_grid.update()
